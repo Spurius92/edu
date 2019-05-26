b16b7c39827504710a60afb24563b1316ae259aa
Spurius92 <bot1412@yandex.ru>
2019-05-26 23:56:09 +0300

    26 may

---
+++ leetCode/easy/removeFromList.py
@@ -0,0 +1,22 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        if not nums:
+            return 0
+        
+        i = 0
+        j = len(nums) - 1
+        
+        while j>= 0 and nums[j] == val:
+            j -= 1      
+            
+        while i < j:
+            if nums[i] == val:
+                nums[i], nums[j] = nums[j], nums[i]
+                
+                while j>=0 and nums[j] == val:
+                    j -= 1
+            i += 1
+        if nums[i] == val:
+            return i 
+        else:
+            return i+1
---
+++ leetCode/easy/valid_parentheses.py
@@ -0,0 +1,16 @@
+# Runtime: 36 ms, faster than 90.03% of Python3 online submissions for Valid Parentheses.
+# Memory Usage: 13.2 MB, less than 61.23% of Python3 online submissions for Valid Parentheses.
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parens = {'(': ')',
+                    '{': '}',
+                    '[': ']'}
+        stack = []
+        for char in s:
+            if char in parens:
+                stack.append(char)
+            else:
+                if not stack or parens[stack.pop()] != char:
+                    return False
+        return False if stack else True
